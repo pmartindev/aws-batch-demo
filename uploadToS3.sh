0965a07ce4d30349c02908b55d78d3b22f7f2b25
pmartindev <prestonmdev@gmail.com>
2019-03-22 08:12:32 -0500

    Added full path to upload

--- uploadToS3.sh
+++ uploadToS3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PATH=~/.local/bin:$PATH
-aws s3 cp helloworld.html s3://ascii-batch-demo/helloworld.html
+aws s3 cp ./aws-batch-demo/helloworld.html s3://ascii-batch-demo/helloworld.html
