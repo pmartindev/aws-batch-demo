c02883e05036ebd949a57b186c7713abddac9dd2
pmartindev <prestonmdev@gmail.com>
2019-03-22 07:03:59 -0500

    Added shebang to uploadToS3.sh

--- buildImage.sh
+++ buildImage.sh
@@ -1,1 +1,4 @@
+$(aws ecr get-login --no-include-email --region us-east-2)
 docker build -t ascii-generator .
+docker tag ascii-generator:latest 324320755747.dkr.ecr.us-east-2.amazonaws.com/ascii-generator:latest
+docker push 324320755747.dkr.ecr.us-east-2.amazonaws.com/ascii-generator:latest
--- uploadToS3.sh
+++ uploadToS3.sh
@@ -1,1 +1,2 @@
+ #!/bin/bash
 aws s3 cp helloworld.html s3://ascii-batch-demo/helloworld.html
